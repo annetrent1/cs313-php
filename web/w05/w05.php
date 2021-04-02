da62342d4d4ced28df2d32b502b20f1246815494
Anne Trent <anne.trent1@gmail.com>
2021-04-02 13:02:31 -0600

    w05 inti

---
+++ web/w05/w05.php
@@ -0,0 +1,21 @@
+<?php require "dbConnect.php";
+$db = get_db();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="./w05.css">
+</head>
+<body>
+
+    <div class="title">
+        <h1>Notecards</h1>
+    </div>
+
+    <div class="section">
+        
+    </div>
+
+
+</body>
+</html>
