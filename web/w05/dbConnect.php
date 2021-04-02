7591f791d4e0c28a02e9b1922e2e4b4c515da5ba
Anne Trent <anne.trent1@gmail.com>
2021-04-02 13:34:12 -0600

    connect db

--- web/w05/dbConnect.php
+++ web/w05/dbConnect.php
@@ -1,4 +1,5 @@
 <?php
+function get_db() {
     $db = NULL
 	try
 	{
@@ -6,26 +7,24 @@
         amazonaws.com:5432/da93e0gi4o6im');
 		$dbOpts = parse_url($dbUrl);
 	
-		$dbHost = $dbOpts["host"];
-		$dbPort = $dbOpts["port"];
-		$dbUser = $dbOpts["user"];
-		$dbPassword = $dbOpts["password"];
-		$dbName = ltrim($dbOpts["path"],'/');
-		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		$dbHost = $dbOpts["ec2-54-205-187-125.compute-1.amazonaws.com"];
+		$dbPort = $dbOpts["5432"];
+		$dbUser = $dbOpts["xypjmqpkzlscmu"];
+		$dbPassword = $dbOpts["24437885416e4c873ae74d4cd981752da2ee80926fb1c77adbb91f1cc08d9fb0"];
+        $dbName = ltrim($dbopts["path"],'/');
+        
+        $dbOptions = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword, $dbOptions);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch (PDOException $exc)
 	{
 		echo 'Error!: ' . $exc->getMessage();
-		die();
-	}
+		exit;
+    }
+    return $db;
+}
 	
 
-?>
-<form method="get">
-<input type="text" name="book"/>
-<input type="submit" value="Submit"/>
-</form>
-<?php
-		
 ?>
