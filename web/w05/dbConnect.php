fc4f7e1d17ac6d88e97cb1a5144f78f3b341bcd5
Anne Trent <anne.trent1@gmail.com>
2021-04-02 14:12:45 -0600

    testing

--- web/w05/dbConnect.php
+++ web/w05/dbConnect.php
@@ -3,26 +3,25 @@ function get_db() {
     $db = NULL
 	try
 	{
-		$dbUrl = getenv('postgres://xypjmqpkzlscmu:24437885416e4c873ae74d4cd981752da2ee80926fb1c77adbb91f1cc08d9fb0@ec2-54-205-187-125.compute-1.
-        amazonaws.com:5432/da93e0gi4o6im');
-		$dbOpts = parse_url($dbUrl);
-	
-		$dbHost = $dbOpts["ec2-54-205-187-125.compute-1.amazonaws.com"];
-		$dbPort = $dbOpts["5432"];
-		$dbUser = $dbOpts["xypjmqpkzlscmu"];
-		$dbPassword = $dbOpts["24437885416e4c873ae74d4cd981752da2ee80926fb1c77adbb91f1cc08d9fb0"];
-        $dbName = ltrim($dbopts["path"],'/');
-        
-        $dbOptions = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+		$dbUrl = getenv('DATABASE_URL');
 
-		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword, $dbOptions);
+		$dbOpts = parse_url($dbUrl);
+
+		$dbHost = $dbOpts["host"];
+		$dbPort = $dbOpts["port"];
+		$dbUser = $dbOpts["user"];
+		$dbPassword = $dbOpts["pass"];
+		$dbName = ltrim($dbOpts["path"],'/');
+
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
-	catch (PDOException $exc)
+	catch (PDOException $ex)
 	{
-		echo 'Error!: ' . $exc->getMessage();
-		exit;
-    }
+		echo 'Error!: ' . $ex->getMessage();
+		die();
+	}
     return $db;
 }
 	
