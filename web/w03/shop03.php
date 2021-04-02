c5c006a5360f6baadd640ea8cdfb28a9806caeb5
Anne Trent <anne.trent1@gmail.com>
2021-04-02 10:28:54 -0600

    testing cart

---
+++ web/w03/shop03.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="./shop03.css">
+    </head>
+<body> 
+
+<h1>Plant Shop</h1>
+<?php echo "hello" ?>
+
+
+    <div class="items_container">
+        <div class="item">
+            <p class="name">Bird of paradise</p>
+            <div class="quantity_container">
+                <label for="bird_of_paradise">Quantity: </label>
+                
+            </div>
+        </div>
+
+        <div class="item">
+            <p class="name">Monstera Deliciosa</p>
+            <div class="quantity_container">
+                <label for="monstera">Quantity: </label>
+                
+            </div>
+        </div>
+        
+        <div class="item">
+            <p class="name">Marble Queen Pothos</p>
+            <div class="quantity_container">
+                <label for="marble_queen_pothos">Quantity: </label>
+            </div>
+        </div>
+
+        <div class="item">
+            
+            <p class="name">Fiddle Leaf Fig</p>
+            <div class="quantity_container">
+                <label for="fiddle_leaf">Quantity: </label>
+           
+            </div>
+        </div>
+
+        <div class="item">
+            
+            <p class="name">Ficus Elastica Burgundy</p>
+            <div class="quantity_container">
+                <label for="rubbber">Quantity: </label>
+               
+            </div>
+        </div>
+
+        <div class="item">
+            
+            <p class="name">Snake Plant</p>
+            <div class="quantity_container">
+                <label for="snake">Quantity: </label>
+                
+            </div>
+        </div>
+
+    </div>
+    <input type="submit" value="Checkout">
+</form>
+
+
+
+</body>
+</html>
