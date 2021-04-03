8ae425f1862491d7e683fcf72512fd3e44aa4566
Anne Trent <anne.trent1@gmail.com>
2021-04-02 21:35:40 -0600

    testing

--- web/w05/login.php
+++ web/w05/login.php
@@ -15,11 +15,12 @@ if(isset($_POST['login'])){
         $_SESSION["sql_query"] = $sql_query; 
         
 
-        echo "<script> console.log('test'); </script>";
+        
         if($sql_query > 0){
             $userid = $db->prepare("select userid from public.user where email='$email' and password='$password';");
             $userid->execute();
             $_SESSION["userid"] = $userid; 
+            echo "<script> console.log('test'); </script>";
             // setcookie('userid', $userid, time() + (86400), "/");
             // header('Location: ./stacks.php');
         }else{
