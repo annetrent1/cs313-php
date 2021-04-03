75086b7df8c3443e11c88e04b582de687ff56c3a
Anne Trent <anne.trent1@gmail.com>
2021-04-03 14:11:07 -0600

    testing

--- web/w06/newNotecard.php
+++ web/w06/newNotecard.php
@@ -29,11 +29,11 @@
         <form method="post" action="" >
             <div class="input-container">
                 <div class="input-section">
-                    <label for="addstack">Front  </label>
+                    <label for="descriptionfront">Front</label>
                     <input class="input" type="text" id="descriptionfront" name="descriptionfront"/>
                 </div>
                 <div class="input-section">
-                    <label for="addstack">Back  </label>
+                    <label for="descriptionback">Back</label>
                     <input class="input" type="text" id="descriptionback" name="descriptionback"/>
                 </div>
                 <div class="button-section">
