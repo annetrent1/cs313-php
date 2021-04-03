151ef6154417ecf56f1801250f50a53f3ced02b2
Anne Trent <anne.trent1@gmail.com>
2021-04-03 13:56:53 -0600

    testing

--- web/w06/newNotecard.php
+++ web/w06/newNotecard.php
@@ -1,5 +1,6 @@
 <?php
-    $stackid = $_POST['stackid'];
+    // $stackid = $_POST['stackid'];
+    $stackid = 1;
     require "./dbConnect.php";
     $db = get_db();
     
@@ -12,5 +13,30 @@
     // $query->execute();
     // header("Location: ./stacks.php");
     // die();
-    echo "$stackid";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="./notecard.css">
+</head>
+<body>
+
+    <div class="title">
+        <h1>Create New Notecard</h1>
+    </div>
+
+    <div class="section">
+        <form method="post" action="" >
+            <div class="item">
+                <label for="addstack">Front</label>
+                <input class="input" type="text" id="descriptionfront" name="descriptionfront"/>
+                <label for="addstack">Back</label>
+                <input class="input" type="text" id="descriptionback" name="descriptionback"/>
+                <input class="add-button" type="submit" value="Submit"/>
+            </div>
+        </form>
+    </div>
+
+
+</body>
+</html>
