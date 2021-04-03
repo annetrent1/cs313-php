25c4aaae95fb0cea1abdb17518f7b807d0270f6d
Anne Trent <anne.trent1@gmail.com>
2021-04-03 15:27:34 -0600

    w6

--- web/w06/newNotecard.php
+++ web/w06/newNotecard.php
@@ -2,7 +2,7 @@
     $stackid = $_POST['stackid'];
     // session_start();
     // $_SESSION['stackid'] = $stackid;
-    // $id = '1';
+    $id = $stackid;
     // $stackid = '1';
     require "./dbConnect.php";
     $db = get_db();
@@ -15,7 +15,7 @@
             VALUES ( ( SELECT stackid FROM public.stack 
             WHERE stackid=:stackid), 
             :front, :back);");
-            $query->bindValue(':stackid', $stackid, PDO::PARAM_INT);
+            $query->bindValue(':stackid', $id, PDO::PARAM_INT);
             $query->bindValue(':front', $front, PDO::PARAM_STR);
             $query->bindValue(':back', $back, PDO::PARAM_STR);
             $query->execute();
