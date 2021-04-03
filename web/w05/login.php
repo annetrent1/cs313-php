4b9b8839e54fb6db8483cc6ad4ac09d9e30fbddd
Anne Trent <anne.trent1@gmail.com>
2021-04-02 18:02:44 -0600

    testing

--- web/w05/login.php
+++ web/w05/login.php
@@ -18,7 +18,7 @@ if(isset($_POST['login'])){
         $count = $row['cntUser'];
 
         if($count > 0){
-            $_SESSION['email'] = $email;
+            // $_SESSION['email'] = $email;
             header('Location: ./notecard.php');
         }else{
             echo "Invalid username and password";
