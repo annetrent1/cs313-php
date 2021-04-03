ad76fcba24deb72584ea86dffbd77499b4dc148d
Anne Trent <anne.trent1@gmail.com>
2021-04-02 20:29:14 -0600

    testing

--- web/w05/notecard.php
+++ web/w05/notecard.php
@@ -14,7 +14,7 @@ $db = get_db();
     </div>
 
     <div class="section">
-        <?php foreach ($db->query("SELECT name FROM public.stack WHERE userid='1'";) as $row)
+        <?php foreach ($db->query("SELECT name FROM public.stack WHERE userid='1';") as $row)
         {
         echo 'stacks: ' . $row['stack'];
         echo '<br/>';
