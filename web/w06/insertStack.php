36f3b40dcca0302b670640f193decb616aac4f4c
Anne Trent <anne.trent1@gmail.com>
2021-04-03 13:23:28 -0600

    testing

--- web/w06/insertStack.php
+++ web/w06/insertStack.php
@@ -6,8 +6,8 @@
     
     $query = $db->prepare("INSERT INTO public.stack (userID, name)
         VALUES ( ( SELECT userID FROM public.user 
-        WHERE userid=':userid'), 
-        ':name');");
+        WHERE userid=:userid), 
+        :name);");
     $query->bindValue(':userid', $userid, PDO::PARAM_INT);
     $query->bindValue(':name', $name, PDO::PARAM_STR);
     $query->execute();
