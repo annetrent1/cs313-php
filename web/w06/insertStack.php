29dfac1ace9c03a5de40fad3b2b372b1b1d02fe6
Anne Trent <anne.trent1@gmail.com>
2021-04-03 13:12:14 -0600

    testing

--- web/w06/insertStack.php
+++ web/w06/insertStack.php
@@ -4,12 +4,12 @@
     require "./dbConnect.php";
     $db = get_db();
     
-    $query = $db->prepare("INSERT INTO public.stack (userID, name)
-        VALUES ( ( SELECT userID FROM public.user 
-        WHERE userid=':userid'), :name ");
-    $query->bindValue(':userid', $userid, PDO::PARAM_INT);
-    $query->bindValue(':name', $name, PDO::PARAM_INT);
-    $query->execute();
+    // $query = $db->prepare("INSERT INTO public.stack (userID, name)
+    //     VALUES ( ( SELECT userID FROM public.user 
+    //     WHERE userid=':userid'), :name ");
+    // $query->bindValue(':userid', $userid, PDO::PARAM_INT);
+    // $query->bindValue(':name', $name, PDO::PARAM_INT);
+    // $query->execute();
     echo "$name";
     echo "$userid";
 ?>
