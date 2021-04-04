46c0704b50a1387aa23fc547e712bdccb74a6a8a
Anne Trent <anne.trent1@gmail.com>
2021-04-03 20:11:07 -0600

    cleanup

--- web/w06/editStack.php
+++ web/w06/editStack.php
@@ -1,5 +1,7 @@
 <?php
-   
+   require "./dbConnect.php";
+   $db = get_db();
+   $stackid = $_GET['stackid'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,7 +9,7 @@
     <link rel="stylesheet" href="./notecard.css">
 </head>
 <body>
-
+    <?php echo $stackid; ?>
 
 </body>
 </html>
