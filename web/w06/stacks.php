47c7a53110217320ec27a8e7702248fda6479b0b
Anne Trent <anne.trent1@gmail.com>
2021-04-03 20:29:16 -0600

    cleanup

--- web/w06/stacks.php
+++ web/w06/stacks.php
@@ -24,7 +24,7 @@
         {
             echo '<p class="item note-item">';
             echo '<a href="./notecards.php?stackid=' . $row['stackid'] . '">' .  $row['name'] . '</a>';
-            echo '<a href="./editStack.php?stackid="' . $row['stackid'] . '"?name="' . $row['name'] . '""> Edit </a>';
+            echo '<a href="./editStack.php?stackid=' . $row['stackid'] . '&name=' . $row['name'] . '"> Edit </a>';
             echo '</p>';
         }
         ?>
