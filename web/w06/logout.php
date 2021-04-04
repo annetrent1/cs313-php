702d1cc15d2f92f7e446da77b39f57a021a4a223
Anne Trent <anne.trent1@gmail.com>
2021-04-03 19:17:16 -0600

    cleanup

--- web/w06/logout.php
+++ web/w06/logout.php
@@ -1,3 +1,27 @@
-<?php 
-    echo "HELLO";
+<?php
+    require "./dbConnect.php";
+    $db = get_db();
+
+    if(isset($_POST['logout'])) {
+            echo "HELLO";
+        }
+    }
+    
+  
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="./logout.css">
+</head>
+<body>
+
+    <div class="section">
+        <form method="post" action="" >
+            <input class="logout-button" type="submit" name="logout" id="logout" value="Logout"/>
+        </form>
+    </div>
+
+
+</body>
+</html>
